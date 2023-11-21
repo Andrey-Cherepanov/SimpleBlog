f964c14235d518d86cd1c88214e0833fda96e24c
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-11-22 00:00:01 +0300

    shair post functional

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 def post_list(request):
     post_list = Post.published.all()
@@ -34,11 +36,21 @@ def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-
+            post_url = request.build_absolute_url(
+                        post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [cd['to']])
+            sent = True
+        
 
     else:
         form = EmailPostForm()
