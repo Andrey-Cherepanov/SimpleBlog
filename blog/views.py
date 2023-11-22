16b7b3917da94db49715aaea89c54d592ce370b8
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-11-22 18:23:11 +0300

    views for similar posts

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -26,7 +27,7 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
         posts = paginator.page(1)
-        
+
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts,
@@ -42,11 +43,19 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
+    # Similar tags
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post':post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # get post by id
