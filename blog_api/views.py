b7ad21f2ea4d4edc53b3d6064ef5fbd4cc474650
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-12-08 20:17:29 +0300

    add filters, orderings and pagination to API listing

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,10 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from blog.models import Post
 from .serializers import PostSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter]
+    filterset_fields = ['author']
+    search_fields = ['body', 'title', 'author__username']
+    ordering_fields = ['author_id', 'publish']
 
     #def get_queryset(self):
     #    user = self.request.user
